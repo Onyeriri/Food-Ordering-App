37d0af0c12d10002ae89589e64b18414a064e7c4
Onyeriri Victor <viktorchinonso@gmail.com>
2023-01-10 06:23:01 +0000

    Added more reducer logic to the store

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,8 +9,12 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
