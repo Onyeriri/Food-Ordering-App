57d9df9bafce9c2552d9189a5b42910ced14ff35
Onyeriri Victor <viktorchinonso@gmail.com>
2023-01-23 13:03:34 +0000

    Added loading effect to available component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,26 +5,28 @@ import MealItem from "./mealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-
-  const fetchData = async () => {
-    const response = await fetch(
-      "https://react-http-d9cc6-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
-    );
-    const data = await response.json();
-    const receivedMeals = [];
-
-    for (const key in data) {
-      receivedMeals.push({
-        id: key,
-        name: data[key].name,
-        description: data[key].description,
-        price: data[key].price,
-      });
-    }
-    setMeals(receivedMeals);
-  };
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(!isLoading);
+      const response = await fetch(
+        "https://react-http-d9cc6-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const data = await response.json();
+      const receivedMeals = [];
+
+      for (const key in data) {
+        receivedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(receivedMeals);
+      setIsLoading(isLoading);
+    };
     fetchData();
   }, []);
 
@@ -32,9 +34,11 @@ const AvailableMeals = () => {
     <section className={classes.meals}>
       <Cards>
         <ul>
-          {meals.map((meal) => (
-            <MealItem key={meal.id} meal={meal} />
-          ))}
+          {isLoading ? (
+            <p>Fetching data...</p>
+          ) : (
+            meals.map((meal) => <MealItem key={meal.id} meal={meal} />)
+          )}
         </ul>
       </Cards>
     </section>
